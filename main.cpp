bae66463e335c3fdef088157a40e1ab8cccb769f
Juan Pablo Vanegas <juan.vanegas@tektontech.com>
2020-07-27 23:48:43 -0500

    Feat: Add paralelization

--- main.cpp
+++ main.cpp
@@ -2,23 +2,38 @@
  * Main module. 
  * This program is for calculating and saving data. For animations please use animate.cpp
  */
+#include <string>
+#include <sstream>
+#include <fstream>
+
 #include "molecular_dynamics.h"
+
+std::string filename(int n);
 
 int main(int argc, char *argv[])
 {
-    Body Molecule[N];
+    int max = 8*10; // number of cores * number of temperatures per core. Change this accordingly
+    double T_0 = 1.0;
+    double Tmax = 1.2, dT = T_max/(double)max;
 
-    MolecularDynamics LennardJones;
-    CRandom ran64(seed);
-    int tSteps = (int)(tMax / dt);
-    double vMax = 0.5;
-    double L, xi0, U, m2, m4, auxM2, auxM4;
-    double Q = 0.2;
-
-    std::cout << "T,L\n";
-
-    for (double T = 1; T < 1.2; T += 1)
+    #pragma omp parallel for shared(T_0, Tmax, dT)
+    for(int n=0; n<max; n++)
     {
+        double T = T_0 + dT*n;
+
+        Body Molecule[N];
+
+        MolecularDynamics LennardJones;
+        CRandom ran64(seed);
+        int tSteps = (int)(tMax / dt);
+        double vMax = 0.5;
+        double L, xi0, U, m2, m4, auxM2, auxM4;
+        double Q = 0.2;
+
+        std::ofstream File(filename(n));
+
+        File << "T,L\n";
+
         //m2 = 0;
         //m4 = 0;
         L = 0;
@@ -53,10 +68,17 @@ int main(int argc, char *argv[])
         //m2 /= (tSteps - (tEq / dt));
         //m4 /= (tSteps - (tEq / dt));
         // U = m4 / (m2 * m2);
-        std::cout << T << ',' << L << std::endl;
+        File << T << ',' << L << std::endl;
+        File.close()
     }
-
     // std::cout << "t,x,y,z,vx,vy,vz,u\n";
 
     return 0;
+}
+
+std::string filename(int n)
+{
+    std::string name; std::stringstream n_s;  n_s << n;
+    name = "./" + n_s + ".csv";
+    return name;
 }
